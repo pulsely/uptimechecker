2367cd2e282dfb71ab2519f5591f3f203c9e865b
pulsely <opensource@pulsely.com>
2022-09-23 23:45:44 +0100

    add settings page for configurations

--- panel/views.py
+++ panel/views.py
@@ -11,9 +11,9 @@ from django.views.decorators.cache import never_cache
 from django.core.exceptions import ObjectDoesNotExist
 
 from uptimecheckcore.components.credentials.privileges import operator_privilege_check, IsOperatorAuthenticated
-#from rest_framework_simplejwt.authentication import JWTAuthentication
+# from rest_framework_simplejwt.authentication import JWTAuthentication
 
-#from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import (
     api_view,
     renderer_classes,
@@ -29,54 +29,73 @@ from uptimebot import models
 from uptimecheckcore.components.helpers.configurations import is_secretkey_insecure
 from uptimebot.handler import check_domains, check_domain
 
+
 @never_cache
 @user_passes_test(operator_privilege_check)
 def index(request):
+    return render(request, "panel/index.html",
+                  {
+                      'title': 'Dashboard',
+                      'is_secretkey_insecure': is_secretkey_insecure,
+                  })
 
-    return render( request, "panel/index.html",
-                              {
-                                  'title' : 'Dashboard',
-                                  'is_secretkey_insecure' : is_secretkey_insecure,
-                              })
 
 @never_cache
 @user_passes_test(operator_privilege_check)
 def settings_(request):
-    return render( request, "panel/settings.html",
-                              {
-                                  'title': 'Settings',
+    return render(request, "panel/settings.html",
+                  {
+                      'title': 'Settings',
 
-                                  'is_secretkey_insecure' : is_secretkey_insecure,
-                              })
+                      'is_secretkey_insecure': is_secretkey_insecure,
+
+                      'DATABASES': settings.DATABASES,
+
+                      'DEBUG_': settings.DEBUG,
+
+                      'ALLOWED_HOSTS_': settings.ALLOWED_HOSTS,
+
+                      'REDIS_URL_': settings.REDIS_URL,
+
+                      'CELERY_TIMEZONE_': settings.CELERY_TIMEZONE,
+
+                      'CELERY_TASK_TRACK_STARTED_': settings.CELERY_TASK_TRACK_STARTED,
+                      'CELERY_TASK_TIME_LIMIT_': settings.CELERY_TASK_TIME_LIMIT,
+
+                      'DEFAULT_PERIODIC_MINUTES_': settings.DEFAULT_PERIODIC_MINUTES,
+                      'DEFAULT_ADD_RANDOMNESS' : settings.DEFAULT_ADD_RANDOMNESS,
+
+                  })
 
 
 @never_cache
 @user_passes_test(operator_privilege_check)
 def users(request):
-    return render( request, "panel/users.html",
-                              {
-                                  'title': 'Users',
+    return render(request, "panel/users.html",
+                  {
+                      'title': 'Users',
 
-                                  'is_secretkey_insecure' : is_secretkey_insecure,
-                              })
+                      'is_secretkey_insecure': is_secretkey_insecure,
+                  })
 
-@api_view(['GET', 'POST',])
-@authentication_classes(( SessionAuthentication, ))
+
+@api_view(['GET', 'POST', ])
+@authentication_classes((SessionAuthentication,))
 @permission_classes((IsOperatorAuthenticated,))
 def api_uptime_list(request):
     results = []
 
-    for s in models.TargetWebsite.objects.order_by('url',):
+    for s in models.TargetWebsite.objects.order_by('url', ):
         try:
             previous_exception_ = models.UptimeCheck.objects.filter(target_website=s,
-                                                                   status__in=['error', 'exception',
-                                                                               'missing_keyword']).order_by(
+                                                                    status__in=['error', 'exception',
+                                                                                'missing_keyword']).order_by(
                 '-start_time', )[0]
 
             previous_exception = {
-                'time' : previous_exception_.end_time.timestamp(),
-                'status' : previous_exception_.status,
-                'status_display' : previous_exception_.get_status_display()
+                'time': previous_exception_.end_time.timestamp(),
+                'status': previous_exception_.status,
+                'status_display': previous_exception_.get_status_display()
             }
         except:
             previous_exception = None
@@ -84,36 +103,37 @@ def api_uptime_list(request):
 
         # models.UptimeCheck.objects.
         uptimes = []
-        for u in models.UptimeCheck.objects.filter(target_website=s).order_by( '-start_time', )[:5]:
-            uptimes.append( {
-                'id' : u.id,
-                'status' : u.status,
-                'status_code' : u.status_code,
-                'status_display' : u.get_status_display(),
-                'end_time_display' : f'{u.end_time}',
-                'end_time' : u.end_time.timestamp()
-            } )
+        for u in models.UptimeCheck.objects.filter(target_website=s).order_by('-start_time', )[:5]:
+            uptimes.append({
+                'id': u.id,
+                'status': u.status,
+                'status_code': u.status_code,
+                'status_display': u.get_status_display(),
+                'end_time_display': f'{u.end_time}',
+                'end_time': u.end_time.timestamp()
+            })
         timestamp = None
         if s.ssl_expire_time:
             timestamp = s.ssl_expire_time.timestamp()
 
         results.append({
-            'id' : f'{s.id}',
-            'title' : s.title,
+            'id': f'{s.id}',
+            'title': s.title,
             'target_website_url': f'{s.url}',
-            'ssl_expire_time' : timestamp,
+            'ssl_expire_time': timestamp,
             'uptimes': uptimes,
-            'previous_exception' : previous_exception,
+            'previous_exception': previous_exception,
         })
 
     return JsonResponse({
-        'results' : results,
-        'status' : 'okay'
+        'results': results,
+        'status': 'okay'
     })
 
-@api_view(['GET', 'POST',])
-#@authentication_classes((JWTAuthentication, SessionAuthentication))
-@authentication_classes(( SessionAuthentication, ))
+
+@api_view(['GET', 'POST', ])
+# @authentication_classes((JWTAuthentication, SessionAuthentication))
+@authentication_classes((SessionAuthentication,))
 @permission_classes((IsOperatorAuthenticated,))
 def api_trigger_refresh(request):
     if settings.DEBUG:
@@ -127,8 +147,8 @@ def api_trigger_refresh(request):
             message = "Refresh all triggered"
 
             return JsonResponse({
-                'message' : message,
-                'status' : 'okay'
+                'message': message,
+                'status': 'okay'
             })
         elif request.data["mode"] == "single" and "host_id" in request.data:
             host_id = request.data["host_id"]
@@ -147,20 +167,19 @@ def api_trigger_refresh(request):
 
             message = "Refresh for single website triggered"
             return JsonResponse({
-                'message' : message,
-                'status' : 'okay'
+                'message': message,
+                'status': 'okay'
             })
-
-
 
     return JsonResponse({
         'error': "Invalid trigger request",
         'status': 'error'
     })
 
+
 @never_cache
 @user_passes_test(operator_privilege_check)
 def crm_logout(request):
     logout(request)
 
-    return HttpResponseRedirect("/") # reverse("landing:index")
+    return HttpResponseRedirect("/")  # reverse("landing:index")
