7d0f2a143594ffe1586a23e5e72df847a14dde7c
pulsely <opensource@pulsely.com>
2023-05-06 22:04:15 +0100

    Add first run

---
+++ uptimecheckcore/components/helpers/first_run.py
@@ -0,0 +1,20 @@
+import os
+
+
+def create_default_file():
+    f = open(".env", "rw")
+
+    f.write("PYTHONDONTWRITEBYTECODE=true")
+    f.write("DEBUG=true")
+    f.write("ALLOWED_HOSTS="*"")
+    f.write('DEFAULT_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"')
+    f.write("DEFAULT_PERIODIC_MINUTES=7")
+    f.close()
+    '''
+PYTHONDONTWRITEBYTECODE=true
+DEBUG=true
+SECRET_KEY="n$0gdvf=s&oqa4ap99%@tyu7m$dey4=(3#e^h$_!2qd%bk2m$)"
+ALLOWED_HOSTS="*"
+DEFAULT_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+DEFAULT_PERIODIC_MINUTES=7
+    '''
