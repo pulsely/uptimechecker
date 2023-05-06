267d7d22a38d64675ff6fc984729cc7044381ec5
pulsely <opensource@pulsely.com>
2023-05-06 22:06:43 +0100

    Update default file

--- uptimecheckcore/components/helpers/first_run.py
+++ uptimecheckcore/components/helpers/first_run.py
@@ -5,9 +5,13 @@ def create_default_file():
     f = open(".env", "a")
 
     f.write("PYTHONDONTWRITEBYTECODE=true")
+    f.write("\n")
     f.write("DEBUG=true")
+    f.write("\n")
     f.write('ALLOWED_HOSTS="*"')
+    f.write("\n")
     f.write('DEFAULT_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"')
+    f.write("\n")
     f.write("DEFAULT_PERIODIC_MINUTES=7")
     f.close()
     '''
